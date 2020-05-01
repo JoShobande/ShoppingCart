951b973ccdd8e2b76085fd4a75e24ff03604c093
Josephine <shobande.josephin@gmail.com>
2020-05-01 21:31:16 +0100

    update

--- src/Components/CartInfo.js
+++ src/Components/CartInfo.js
@@ -1,6 +1,31 @@
 import React from 'react';
 
 
+const DropDownInfo = () =>{
+    return(
+        <div>
+            <div className="info-wrapper">
+                <div className="flex-box">
+                    <div  className="flex-box_two">
+                        <div>
+                            <img src="../images/orange.jpeg" alt="" />
+                        </div>
+                        <div>
+                            <p>orange - 1kg <br/>$102</p>
+                        </div>
+                    </div>
+                   
+                    <div>
+                        <p>1 No. <br/> $102</p>
+                    </div>
+                </div>
+               
+            </div>
+        </div>
+    )
+}
+
+
 const CartInfo =()=>{
     return(
         <div className="flex-box">
@@ -12,6 +37,7 @@ const CartInfo =()=>{
             <div className="bag-icon">
                 <img src="./images/bag.png" alt="bag"/>
             </div>
+            <DropDownInfo />
         </div>
         
     )
