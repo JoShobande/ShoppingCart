9a39e047a372db8d5d26168b3cb3694fb979dac1
Josephine <shobande.josephin@gmail.com>
2020-05-05 15:22:28 +0100

    update

--- src/Components/CartInfo.js
+++ src/Components/CartInfo.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+const DropDownEmpty = ()=>{
+    return(
+        <div className="empty_wrapper">
+            <img src="https://res.cloudinary.com/sivadass/image/upload/v1495427934/icons/empty-cart.png" alt="empty-cart"/>
+            <p className="empty_text">Your cart is empty Love!</p>
+        </div>
 
-const DropDownInfo = () =>{
+    )
+}
+
+// const DropDownInfo =()=>{
+//     return(
+//         <div>
+        
+//         </div>
+//     )
+// }
+
+const DropDownButton = ()=>{
+    return(
+        <div className="button_wrapper">
+            <button>PROCEED TO CHECKOUT</button>
+        </div>
+    )
+}
+
+const DropDown = () =>{
     return(
         <div>
             <div className="info-wrapper">
-                <div className="flex-box">
-                    <div  className="flex-box_two">
-                        <div>
-                            <img src="../images/orange.jpeg" alt="" />
-                        </div>
-                        <div>
-                            <p>orange - 1kg <br/>$102</p>
-                        </div>
-                    </div>
-                   
-                    <div>
-                        <p>1 No. <br/> $102</p>
-                    </div>
-                </div>
+               
+                <DropDownEmpty/>
+
+                <DropDownButton/>
                
             </div>
         </div>
@@ -27,7 +42,13 @@ const DropDownInfo = () =>{
 
 
 const CartInfo =()=>{
+
+    const [dropDown, toggleDropDown] = useState(false)
+
     return(
+
+        
+        
         <div className="flex-box">
             <div className="subtotal-section">
                 <p>No. Of Items : 0 <br></br>
@@ -35,9 +56,11 @@ const CartInfo =()=>{
                     
             </div>   
             <div className="bag-icon">
-                <img src="./images/bag.png" alt="bag"/>
+                <img onClick={()=>toggleDropDown(!dropDown)} src="./images/bag.png" alt="bag"/>
+
+                {dropDown ? <DropDown/> : null}
             </div>
-            <DropDownInfo />
+            
         </div>
         
     )
@@ -45,3 +68,18 @@ const CartInfo =()=>{
 
 
 export default CartInfo;
+
+ // <div className="flex-box">
+                //     <div  className="flex-box_two">
+                //         <div>
+                //             <img src="../images/orange.jpeg" alt="" />
+                //         </div>
+                //         <div>
+                //             <p>orange - 1kg <br/>$102</p>
+                //         </div>
+                //     </div>
+                   
+                //     <div>
+                //         <p>1 No. <br/> $102</p>
+                //     </div>
+                // </div>
